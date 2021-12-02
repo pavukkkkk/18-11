eedf2f2bb95c55ded465a76d923222a426270810
KonstantinK <89852159+pavukkkkk@users.noreply.github.com>
2021-12-02 08:49:33 +0400

    Update 1.cpp

--- 1/1.cpp
+++ 1/1.cpp
@@ -1,32 +1,50 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
-
-
-string f(int x1, int y1, int x2, int y2) {
-	if (y1 == y2 && x1==x2) {
-		return "tochka";
-	}
-	if (y1 == y2) {
-		return "gorizontalno";
-	}
-	if (x1 == x2) {
-		return "vertikalno";
-	}
-	else {
-		return "naklonno";
-	}
+double eps = 1e-9;
+int ng=0,nn=0,nv=0,nt=0;
+double middle(double x1, double x2)
+{
+return (x1 + x2) / 2.;
 }
+
+void f(int x1, int y1, int x2, int y2) {
+    if (abs(y1 -y2)<eps && abs(x1-x2)<eps) {
+        cout <<"tochka"<<endl;
+        nt++;
+    }
+    else if (abs(y1 -y2)<eps) {
+        cout <<"gorizontalno"<<endl;
+        ng++;
+    }
+    else if (abs(x1-x2)<eps) {
+       cout<<"vertikalno"<<endl;
+        nv++;
+    }
+    else {
+        double mx= middle (x1,x2);
+        double my =middle (x1,x2);
+        cout<< "naklonno"<<" "<<"seredina: "<<mx<<" "<<my<<endl;
+        nn++;
+    }
+}
+
 
 int main()
 {
-	int n;
-	int x1, x2, y1, y2;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> x1 >> y1 >> x2 >> y2;
-		cout << f(x1, y1, x2, y2)<<endl;
+    
+    int n;
+    int x1, x2, y1, y2;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x1 >> y1 >> x2 >> y2;
+        f(x1, y1, x2, y2);
+        
 
-		
-
-	}
+    }
+    cout<<"tochka: "<<nt<<endl;
+    cout<<"vertikalno: "<<nv<<endl;
+    cout<<"gorizontalno: "<<ng<<endl;
+    cout<<"naklonno: "<<nn<<endl;
 }
+
+//исправленныйивариант, старый в предыдщем в коммите
